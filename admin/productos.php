6c243808ffe1303aa8be5f43e92a4ef436a6fa11
LilFranklo66 <52769400+LilFranklo66@users.noreply.github.com>
2021-04-07 19:26:02 -0600

    Eliminar Pasado de rielk
    1 2 3 4 5 6 7 8 9 10
    Aprendiendo a contar diez números

--- admin/productos.php
+++ admin/productos.php
@@ -117,7 +117,13 @@
               <td><?php echo $f['descripcion'];?></td>
               <td><?php echo $f['inventario'];?></td>
               <td><?php echo $f['catego'];?></td>
-              <td></td>
+              <td>
+                <button class="btn btn-danger btn-small btnEliminar" 
+                data-id="<?php echo $f['id'];?>"
+                data-toggle="modal" data-target="#modalEliminar">
+                  <i class="fa fa-trash"></i>
+                </button>
+              </td>
             </tr>
             <?php
               }
@@ -179,6 +185,29 @@
       </div>
     </div>
   </div>
+<!-- Eliminar Asi es-->
+  <div class="modal fade" id="modalEliminar" tabindex="-1" role="dialog" aria-labelledby="modalEliminarLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+          <div class="modal-header">
+              <h5 class="modal-title" id="modalEliminarLabel">Eliminar Producto</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+          </div>
+          <div class="modal-body">
+              ¿Desea eliminar el producto?
+          </div>
+          <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+              <button type="sumbit" class="btn btn-danger eliminar" data-dismiss="modal">Eliminar</button>
+          </div> 
+      </div>
+    </div>
+  </div>
+
+
+
   <?php include "./layouts/footer.php";?>
 </div>
 <!-- ./wrapper -->
@@ -217,5 +246,30 @@
 <script src="./dashboard/dist/js/demo.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="./dashboard/dist/js/pages/dashboard.js"></script>
+
+<script>
+$(document).ready(function(){
+  var idEliminar=-1;
+  var fila;
+  $(".btnEliminar").click(function(){
+    idEliminar=$(this).data('id');
+	  fila=$(this).parent('td').parent('tr');
+  });
+  $(".eliminar").click(function(){
+    $.ajax({
+      url: '../php/eliminarproducto.php',
+      method: 'POST',
+      data:{
+        id:idEliminar
+      }
+    }).done(function(res){
+      $(fila).fadeOut(1000);
+    });
+    
+  });
+
+});
+</script>
+
 </body>
 </html>
