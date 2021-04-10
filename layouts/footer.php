00b23a0de55fecc9b06e859fc8dbd61dff0ba125
DESKTOP-MGCDLN9\Just Gaming <ornancastillo@gmail.com>
2021-04-09 19:28:19 -0600

    Corrección footer

--- layouts/footer.php
+++ layouts/footer.php
@@ -64,9 +64,10 @@ p a{
   color: #777;
 }
 
-.letrona a:hover{
-  color: red;
-  text-decoration: none;
+
+
+.sp a:hover{
+  color:red;
 }
 
 </style>
@@ -77,26 +78,23 @@ p a{
       <h5 class="titulosFooter">Acerca de nosotros</h5>
       <p>FreshYam Electronics es una empresa 100% mexicana dedicada a la comercialización y venta de aparatos electrónicos. Visita nuestro catálogo y navega entre la gran cantidad de artículos que tenemos disponibles para ti.</p>
     </div>
-    <div class="col-md-3 letrona">
+    <div class="col-md-4 sp">
       <h5 class="titulosFooter">Redes Sociales</h5>
-      <p class="negras"><a href="https://www.facebook.com/juan.lizarraga.35762"><i class="fa fa-facebook iconosk"></i>Síguenos en nuestro Facebook</a></p>
-      <p class="negras"><a href="https://www.instagram.com/franklox/"><i class="fa fa-instagram iconosk"></i>Síguenos en nuestro Instagram</a></p>
-      <p><a href="https://youtube.com/channel/UC9wGfTqvxrDssPVuBW6NgbQ"><i class="fa fa-youtube-play iconosk"></i>Síguenos en nuestro YouTube</a></p>
+      <p style="text-align:center;" class=""><a href="https://www.facebook.com/juan.lizarraga.35762"><i class="fa fa-facebook iconosk"></i>Síguenos en nuestro Facebook</a></p>
+      <p style="text-align:center;"><a href="https://www.instagram.com/franklox/"><i class="fa fa-instagram iconosk"></i>Síguenos en nuestro Instagram</a></p>
+      <p style="text-align:center;"><a href="https://youtube.com/channel/UC9wGfTqvxrDssPVuBW6NgbQ"><i class="fa fa-youtube-play iconosk"></i>Síguenos en nuestro YouTube</a></p>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-4">
       <h5 class="titulosFooter">Información de Contacto</h5>
-      <p><i class="fa fa-home iconosk"></i>Carretera Municipal Libre Mazatlán Higueras Km 3, 82199 Mazatlán, Sin.</p>
-      <p><i class="fa fa-mobile iconosk"></i>+52 1 694 118 1385</p>
-      <p><i class="fa fa-envelope-o iconosk"></i>2018030374@upsin.edu.mx</p>
+      <p style="text-align:center;"><i class="fa fa-home iconosk"></i>Carretera Municipal Libre Mazatlán Higueras Km 3, 82199 Mazatlán, Sin.</p>
+      <p style="text-align:center;"><i class="fa fa-mobile iconosk"></i>+52 1 694 118 1385</p>
+      <p style="text-align:center;"><i class="fa fa-envelope-o iconosk"></i>2018030374@upsin.edu.mx</p>
     </div>
   </div>
   <div class="container-footer">
-    <div class="footer">
-      <div class="copyrightFooter ">
+    <div class="footer justify-content-center">
+      <div class="copyrightFooter">
         © 2021 Todos los Derechos Reservados | <a href="">FreshYam Electronics</a>
-      </div>
-      <div class="terminos">
-       <a>Información de Compañia</a> | <a>Privacidad y Política</a> | <a>Términos y Condiciones</a>
       </div>
     </div>
   </div>
